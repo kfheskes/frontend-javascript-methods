0fd56202f1f1604918c4d014a7135ce70aeeb9fb
kfheskes <kayarneheskes@hotmail.com>
2023-04-01 12:06:10 +0200

    uitwerkingen01041206

--- string-methods.js
+++ string-methods.js
@@ -13,6 +13,8 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
+const isFruitLonger = fruit.length >vegetable.length;
+console.log(isFruitLonger);
 
 
 /* Opdracht 2 */
@@ -21,14 +23,15 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
-
+console.log(fruit.charAt(fruit.length -1));
+console.log(vegetable.charAt(vegetable.length -1));
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
+console.log(userInput.trim());
 
 
 /* Opdracht 4 */
@@ -36,14 +39,19 @@ const userInput = "  De kat krabt de krullen van de trap ";
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
+console.log(story.includes('Fantastisch'));
 
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
+console.log(story.substring(0,53));
 
 
+// const index = story.indexOf("Fantastisch");
+// const result = story.substring(0, index);
+//
+// console.log(result);
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -54,6 +62,13 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 
 const price = "$35.59";
 
+const newPrice = price.replaceAll('$', '€').replaceAll('.', ',');
+
+console.log(newPrice);
+
+// const otherPrice = price.replace("$", "€").replace(".", ",");
+//
+// console.log(otherPrice);
 
 
 
